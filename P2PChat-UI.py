997770da548f6f4b2deef9a8c3aacc69800942c6
Samson-Dai <dscsamson@gmail.com>
2019-04-04 21:04:18 +0800

    update meta data, add gitignore file

--- P2PChat-UI.py
+++ P2PChat-UI.py
@@ -3,8 +3,8 @@
 # Student name and No.: Zhang Yuqian 3035233565
 # Student name and No.: Dai Songcheng 3035232652
 # Development platform: Ubuntu 16.04
-# Python version:
-# Version:
+# Python version: python 3.6.7
+# Version: 0.1
 
 
 from tkinter import *
